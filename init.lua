fischerling/vis-lspc

commit 9c0a9b4fdcb2f0912b8ec3b9f7127d660565f898

fix initialized notification parameters
LSP defines the parameters of the initialized notifications as empty
object.
The dart language server expects that the initialize notification has an
empty params object.
Therefore, pass an empty parameter table to the initialized notification.

Tested with our fallback json implementation as well as dkjson.

Fixes !37.

init.lua
@@ -863,7 +863,10 @@ local function ls_handle_method_response(ls, method_response, req)
   elseif method == 'initialize' then
     ls.initialized = true
     ls.capabilities = result.capabilities
-    ls_send_notification(ls, 'initialized')
+    params = {}
+    setmetatable(params, {__jsontype = 'object'})
+
+    ls_send_notification(ls, 'initialized', params)
 
   elseif method == 'textDocument/completion' then
     lspc_handle_completion_method_response(win, result, req.ctx)

commit 1ddebc95b7a90c28e3cb5a9e386f6c2fbf251928

use local variable instead of a global one
Fixes: 9c0a9b4fdcb2f0912b8ec3b9f7127d660565f898

init.lua
@@ -863,7 +863,7 @@ local function ls_handle_method_response(ls, method_response, req)
   elseif method == 'initialize' then
     ls.initialized = true
     ls.capabilities = result.capabilities
-    params = {}
+    local params = {}
     setmetatable(params, {__jsontype = 'object'})
 
     ls_send_notification(ls, 'initialized', params)