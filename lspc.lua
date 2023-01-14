6b5676791531a6b465661430077997f41caf84d8
Florian Fischer <florian.fischer@muhq.space>
2023-01-14 17:45:08 +0100

    bump version to 0.1.7

--- lspc.lua
+++ lspc.lua
@@ -5,7 +5,7 @@ local lspc = {
   -- mapping language server names to their state tables
   running = {},
   name = 'vis-lspc',
-  version = '0.1.6',
+  version = '0.1.7',
   -- write log messages to lspc.log_file
   logging = false,
   log_file = nil,
