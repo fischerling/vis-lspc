fischerling/vis-lspc

commit e83950a7149f364b653395cdd4d9446875a7f469

Add textDocument/formatting implementation
This is tested with the Rust language server and the Lua one. Moving
the cursor to the correct position afterwards works with Rust, but not
with Lua. Being smarter about the ranges is probably necessary for Lua
and might make other ones more robust.

init.lua
@@ -365,6 +365,27 @@ local function vis_apply_textEdit(win, file, textEdit)
   win:draw()
 end
 
+-- apply a list of textEdits received from the language server
+local function vis_apply_textEdits(win, file, textEdits)
+  assert(win.file == file)
+
+  local edits = {}
+  for _, textEdit in ipairs(textEdits) do
+    local range = lsp_range_to_vis_range(win, textEdit.range)
+    table.insert(edits, {
+      mark = file:mark_set(range.start),
+      len = range.finish - range.start,
+      newText = textEdit.newText,
+    })
+  end
+  for _, edit in ipairs(edits) do
+    local pos = file:mark_get(edit.mark)
+    file:delete(pos, edit.len)
+    file:insert(pos, edit.newText)
+  end
+  win:draw()
+end
+
 -- concatenate all numeric values in choices and pass it on stdin to lspc.menu_cmd
 local function lspc_select(choices)
   local menu_input = ''
@@ -782,6 +803,10 @@ local function lspc_handle_rename_method_response(win, result)
   vis_apply_workspaceEdit(win, win.file, result)
 end
 
+local function lspc_handle_formatting_method_response(win, result)
+  vis_apply_textEdits(win, win.file, result)
+end
+
 local function lspc_handle_initialize_response(ls, result)
   ls.initialized = true
   ls.capabilities = result.capabilities
@@ -840,6 +865,9 @@ local function ls_handle_method_response(ls, method_response, req)
 
   elseif method == 'textDocument/rename' then
     lspc_handle_rename_method_response(win, result)
+
+  elseif method == 'textDocument/formatting' then
+    lspc_handle_formatting_method_response(win, result)
 
   elseif method == 'shutdown' then
     ls_send_notification(ls, 'exit')
@@ -1296,6 +1324,31 @@ vis:command_register('lspc-rename', function(argv, _, win)
   params.newName = new_name
 
   ls_call_text_document_method(ls, 'rename', params, win)
+end)
+
+vis:command_register('lspc-format', function(_, _, win)
+  local ls, err = lspc_get_usable_ls(win.syntax)
+  if err then
+    lspc_err(err)
+    return
+  end
+
+  -- check if the language server has a provider for this method
+  if not ls.capabilities['documentFormattingProvider'] then
+    lspc_err('language server ' .. ls.name .. ' does not provide formatting')
+    return
+  end
+
+  if not ls.open_files[win.file.path] then
+    lspc_open(ls, win, win.file)
+  end
+
+  local params = {
+    textDocument = {uri = path_to_uri(win.file.path)},
+    options = {tabSize = 2, insertSpaces = true},
+  }
+
+  ls_call_text_document_method(ls, 'formatting', params, win)
 end)
 
 vis:command_register('lspc-completion', function(_, _, win)

commit b1db5ae2a8aeeb72dca127e62d5cbf5b3c7caced

do not index a nil value when no formatting edits are returned
The method textDocument/formatting is allowed to return null/nil which
was directly passed to vis_apply_textEdits causing an exception
when trying to iterate the textEdits table.

Only pass the result of textDocument/formatting to vis_apply_textEdits
if there is an actual result.

Fixes: e83950a7149f364b653395cdd4d9446875a7f469

init.lua
@@ -822,7 +822,10 @@ local function lspc_handle_rename_method_response(win, result)
 end
 
 local function lspc_handle_formatting_method_response(win, result)
-  vis_apply_textEdits(win, win.file, result)
+  -- The result of textDocument/formatting is defined as TextEdit[] | null
+  if result then
+    vis_apply_textEdits(win, win.file, result)
+  end
 end
 
 local function lspc_handle_initialize_response(ls, result)