fischerling/vis-lspc

commit 7099e56062861fc8dbb143e78447cce6bf886a97

fix diagnostic highlighting and message
Some language servers report diagnostics with empty ranges, start and
end are at the same line and character, which currently results in
no highlighting at all.

Since the end of a LSP range is exclusive we always subtract 1 from
it which results in invalid ranges when the server has sent an empty one.
Introduce a check to fix this special case.

`textDocument/publishDiagnostics` doesn't always have a `code` field,
resulting in `diagnostic.code` being nil and so it is replaced with
`diagnostic` in those cases.

init.lua
@@ -575,9 +575,19 @@ local function lspc_highlight_diagnostics(win, diagnostics, style)
   for _, diagnostic in ipairs(diagnostics) do
     if lspc.highlight_diagnostics == 'range' then
       local range = diagnostic.vis_range
+
+      -- LSP ranges use an exclusive finish
+      local finish = range.finish - 1
+
+      -- In some instances the range defined by the diagnostic starts
+      -- and ends at the same position. Highlight the exact position.
+      if range.finish == range.start then
+        finish = range.finish
+      end
+
       -- make sure to highlight only ranges which actually contain the diagnostic
       if diagnostic.content == win.file:content(range) then
-        win:style(style, range.start, range.finish - 1)
+        win:style(style, range.start, finish)
       end
 
     elseif lspc.highlight_diagnostics == 'line' then
@@ -1339,7 +1349,8 @@ local function lspc_show_diagnostic(ls, win, line)
   for _, diagnostic in ipairs(diagnostics_to_show) do
     diagnostics_msg = diagnostics_msg ..
                           string.format(diagnostics_fmt, diagnostic.start.line,
-                                        diagnostic.start.col, diagnostic.code, diagnostic.message)
+                                        diagnostic.start.col, diagnostic.code or 'diagnostic',
+                                        diagnostic.message)
   end
 
   if diagnostics_msg ~= '' then

commit ecd67870e578c061b06ecc4072477da5e9ea9f52

fix wandering empty diagnostic highlights
The last commit introduces support for highlighting empty selections
by properly calculating their vis ranges.
However File:content does return an empty string for empty ranges
and we can not check whether the diagnostic still applies to the same
position.

This is fixed by faking a one character range when receiving an
empty one.
Doing so we get a valid one-character content we can compare and
still have proper highlighting because we unconditionally decrement
the range end in highlight_diagnostics.

Fixes: 7099e56062861fc8dbb143e78447cce6bf886a97

init.lua
@@ -579,12 +579,6 @@ local function lspc_highlight_diagnostics(win, diagnostics, style)
       -- LSP ranges use an exclusive finish
       local finish = range.finish - 1
 
-      -- In some instances the range defined by the diagnostic starts
-      -- and ends at the same position. Highlight the exact position.
-      if range.finish == range.start then
-        finish = range.finish
-      end
-
       -- make sure to highlight only ranges which actually contain the diagnostic
       if diagnostic.content == win.file:content(range) then
         win:style(style, range.start, finish)
@@ -991,6 +985,15 @@ local function lspc_handle_publish_diagnostics(ls, uri, diagnostics)
       -- And because we can't do it during a WIN_HIGHLIGHT events because
       -- lsp_range_to_vis_range modifies the primary selection
       diagnostic.vis_range = lsp_range_to_vis_range(vis.win, diagnostic.range)
+
+      -- In some instances the range defined by the diagnostic starts
+      -- and ends at the same position. Highlight the exact position.
+      if diagnostic.vis_range.finish == diagnostic.vis_range.start then
+        -- We fake a one char range to retrieve its content.
+        -- In highlight_diagnostics we inconditionally decrement finish anyway.
+        diagnostic.vis_range.finish = diagnostic.vis_range.finish + 1
+      end
+
       -- Remember the content of the diagnostic to only highlight it if the content
       -- did not change
       diagnostic.content = vis.win.file:content(diagnostic.vis_range)